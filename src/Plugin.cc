5fe2b933fab1048b36020df1c14b089f2c3e8e8b
Evan Typanski <evan.typanski@corelight.com>
2024-12-12 16:07:52 -0500

    Add dumb NATS key/value example so I don't lose it

--- src/Plugin.cc
+++ src/Plugin.cc
@@ -1,17 +1,61 @@
 
+#include <nats/nats.h>
+
 #include "Plugin.h"
 
-namespace plugin { namespace Storage_Nats { Plugin plugin; } }
+namespace plugin {
+namespace Storage_Nats {
+Plugin plugin;
+}
+} // namespace plugin
 
 using namespace plugin::Storage_Nats;
 
-zeek::plugin::Configuration Plugin::Configure()
-	{
-	zeek::plugin::Configuration config;
-	config.name = "Storage::Nats";
-	config.description = "<Insert description>";
-	config.version.major = 0;
-	config.version.minor = 1;
-	config.version.patch = 0;
-	return config;
-	}
+zeek::plugin::Configuration Plugin::Configure() {
+  zeek::plugin::Configuration config;
+  config.name = "Storage::Nats";
+  config.description = "A NATS backend for key/value stores";
+  config.version.major = 0;
+  config.version.minor = 1;
+  config.version.patch = 0;
+
+  // EXPERIMENT TIME
+  natsConnection *conn = NULL;
+  natsOptions *opts = NULL;
+  natsConnection_Connect(&conn, opts);
+
+  // Initialize and set some JetStream options
+  jsOptions jsOpts;
+  jsOptions_Init(&jsOpts);
+  jsOpts.PublishAsync.MaxPending = 256;
+
+  // Create JetStream Context
+  jsCtx *js = NULL;
+  kvStore *kv = NULL;
+  kvConfig kvc;
+
+  natsConnection_JetStream(&js, conn, &jsOpts);
+
+  // Assume we got a JetStream context in `js`...
+
+  kvConfig_Init(&kvc);
+  kvc.Bucket = "KVS";
+  kvc.History = 10;
+  natsStatus s = js_CreateKeyValue(&kv, js, &kvc);
+
+  // Do some stuff...
+
+  // This is to free the memory used by `kv` object,
+  // not delete the KeyValue store in the server
+  uint64_t rev = 0;
+
+  // Assume we got a kvStore...
+
+  s = kvStore_PutString(&rev, kv, "MY_KEY", "my value");
+
+  // If the one does not care about getting the revision, pass NULL:
+  s = kvStore_PutString(NULL, kv, "MY_KEY", "my value");
+
+  kvStore_Destroy(kv);
+  return config;
+}
